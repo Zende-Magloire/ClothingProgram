21aeb741577c41f15f628bc13adae805617bb710
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2021-11-09 13:36:27 -0500

    day 4 cnt'd

--- src/Clothes.java
+++ src/Clothes.java
@@ -1,4 +1,17 @@
 public abstract class Clothes
 {
+    private String E_Design;
+
 public abstract double price ();
+
+public void Clothes(String d)
+{
+        E_Design = d;
+}
+
+public String getDesign()
+{
+    return E_Design;
+}
+
 }
